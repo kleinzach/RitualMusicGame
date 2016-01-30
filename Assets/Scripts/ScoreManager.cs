e7b5ffc9cf81844aa8dd34819eb75ad2adbfe002
Henry Eastman <henry.eastman@gmail.com>
2016-01-30 09:52:03 -0500

    Starting the fade between music

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class ScoreManager : MonoBehaviour {
 
@@ -20,16 +21,21 @@ public class ScoreManager : MonoBehaviour {
     public float accuracyCutoff;
 
     public Text ComboText;
+    public AudioMixer mixer;
+    public AudioMixerSnapshot snap1;
+    public AudioMixerSnapshot snap2;
+    public AudioMixerSnapshot snap3;
+    public AudioMixerSnapshot snap4;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         score = 0;
         singleton = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        mixer.TransitionToSnapshots(new AudioMixerSnapshot[] { snap1, snap2, snap3, snap3 }, new float[] { 1, 100 * score / 10, 100 * (score-10) / 20, 100 * (score-20) / 30 }, .1f);
 	}
 
     public static void ScoreBead(Bead b, float accuracy)
