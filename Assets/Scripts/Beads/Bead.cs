aba7dccce976d69bf1f56eb63ccb10ca72692abb
JeffMlakar <Jeff.Mlakar@gmail.com>
2016-01-30 16:12:50 -0500

    Adding Reversing

--- Assets/Scripts/Beads/Bead.cs
+++ Assets/Scripts/Beads/Bead.cs
@@ -17,6 +17,8 @@ public abstract class Bead : MonoBehaviour {
     protected AudioSource asource;
     public AudioClip clip;
 
+	public bool IsNext;
+
 	// Use this for initialization
 	void Start () {
         rend = GetComponentInChildren<SpriteRenderer>();
