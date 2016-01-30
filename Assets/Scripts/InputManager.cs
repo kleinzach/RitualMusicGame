55dab817cdba1d4152b87176827adc94c1ec5d75
sn03 <steven.norris@gmail.com>
2016-01-30 00:53:21 -0500

    Add key press logic
    Iterate over lists of keys and rings.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class InputManager : MonoBehaviour {
+    public List<Bead> beadList;
+    public List<KeyCode> keyList;
+    public List<BeatRing> ringList;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        foreach (Bead bead in beadList)
+        {
+            keyList.Add(bead.key);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        List<KeyCode> keyPressedList = new List<KeyCode>();
+        foreach (KeyCode key in keyList)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                keyPressedList.Add(key);
+            }
+                
+        }
+        foreach (BeatRing ring in ringList)
+        {
+            if (keyPressedList.Contains(ring.getCurrentKey()))
+            {
+                keyPressedList.Remove(ring.getCurrentKey());
+                // score points
+            }
+        }
+        foreach (KeyCode key in keyPressedList)
+        {
+            // lose points
+        }
 	}
 }
